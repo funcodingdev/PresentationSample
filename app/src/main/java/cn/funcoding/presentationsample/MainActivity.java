bf8543ef593fcbf3cd08a6b78eadf384c3b9da11
FangJu <funcodingdev@foxmail.com>
2022-03-20 18:19:28 +0800

    init

---
+++ app/src/main/java/cn/funcoding/presentationsample/MainActivity.java
@@ -0,0 +1,28 @@
+package cn.funcoding.presentationsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.bt1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PresentationActivity.class));
+            }
+        });
+        findViewById(R.id.bt2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PresentationWithMediaRouterActivity.class));
+            }
+        });
+    }
+}
